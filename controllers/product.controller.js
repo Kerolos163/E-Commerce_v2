65ec0f89aa6fb941c01dc69e1d76f0d9b90562c2
kerolos <kokoessa951@gmail.com>
2025-09-06 18:37:26 +0300

    feat: implement Product functionality with model, controller, and CRUD operations

---
+++ controllers/product.controller.js
@@ -0,0 +1,107 @@
+const slugify = require("slugify");
+const asyncHandler = require("express-async-handler");
+const Product = require("../models/ProductModel");
+const httpStatus = require("../utils/http_status");
+const AppError = require("../utils/appError");
+
+// @desc Get all Products
+// @route GET /api/v1/Products
+// @access Public
+exports.getAllProducts = asyncHandler(async (req, res, next) => {
+  const limit = +req.query.limit || 10;
+  const page = +req.query.page || 1;
+  const skip = (page - 1) * limit;
+
+  const products = await Product.find(
+    {},
+    { __v: 0, createdAt: 0, updatedAt: 0 }
+  )
+    .skip(skip)
+    .limit(limit);
+
+  res.status(200).json({
+    status: httpStatus.success,
+    page: +page,
+    totalPages: Math.ceil((await Product.countDocuments()) / limit),
+    products,
+  });
+});
+
+// @desc Get product by ID
+// @route GET /api/v1/products/:id
+// @access Public
+exports.getProductById = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const product = await Product.findById(id, {
+    __v: 0,
+    createdAt: 0,
+    updatedAt: 0,
+  });
+  if (!product) {
+    const err = new AppError("Category not found", 404);
+    return next(err);
+  }
+
+  res.status(200).json({
+    status: httpStatus.success,
+    product,
+  });
+});
+
+// @desc Create a new product
+// @route POST /api/v1/products
+// @access Private
+exports.createProduct = asyncHandler(async (req, res, next) => {
+  req.body.slug = slugify(req.body.title);
+
+  const product = new Product(req.body);
+  const result = await product.save();
+  res.status(201).json({
+    status: httpStatus.success,
+    message: "Product created",
+    product: { id: result._id, name: result.name, slug: result.slug },
+  });
+});
+
+// @desc Update a product
+// @route PUT /api/v1/products/:id
+// @access Private
+exports.updateProduct = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  req.body.slug = slugify(req.body.title);
+
+  const product = await Product.findByIdAndUpdate(
+    id,
+    req.body,
+    { new: true }
+  );
+
+  if (!product) {
+    const err = new AppError("Product not found", 404);
+    return next(err);
+  }
+
+  res.status(200).json({
+    status: httpStatus.success,
+    message: "Product updated",
+    product: { id: product._id, name: product.name, slug: product.slug },
+  });
+});
+
+// @desc Delete a product
+// @route DELETE /api/v1/Products/:id
+// @access Private
+exports.deleteProduct = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndDelete(id);
+  if (!product) {
+    const err = new AppError("Product not found", 404);
+    return next(err);
+  }
+
+  res.status(204).json({
+    status: httpStatus.success,
+    message: "Category deleted",
+    product: { id: product._id, name: product.name, slug: product.slug },
+  });
+});
---
+++ models/ProductModel.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [3, "Product title must be at least 3 characters"],
+      maxlength: [100, "Product title must be at most 100 characters"],
+    },
+    slug: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Product description is required"],
+      minlength: [20, "Product description must be at least 20 characters"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Product quantity is required"],
+    },
+    solid: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      trim: true,
+      required: [true, "Product price is required"],
+    },
+    priceAfterDiscount: {
+      type: Number,
+      trim: true,
+      default: 0,
+    },
+    colors: [String],
+    imageCover: {
+      type: String,
+      required: [true, "Image cover is required"],
+    },
+    images: [String],
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Product must belong to a Category"],
+    },
+    subCategory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubCategory",
+      },
+    ],
+    brand: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+    },
+    ratingsAverage: {
+      type: Number,
+      min: [1, "Rating must be above or equal 1.0"],
+      max: [5, "Rating must be below or equal 5.0"],
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
